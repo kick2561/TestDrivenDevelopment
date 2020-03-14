490a3429c69f7181d620028de1f86f70185ec50b
Bonu <cb185249@ncr.com>
2020-03-14 21:07:26 +0530

    Test

---
+++ Stack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    public class Stack<T>
+    {
+        private T[] stackArray;
+        private int maximumLength;
+
+        public int Size { get; private set; }
+
+        public Stack(int length)
+        {
+            maximumLength = length;
+            stackArray = new T[length];
+        }
+    }
+}
